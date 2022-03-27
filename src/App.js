0b6c948f582c4070e14428d942f6ba3351c96ca6
McAsh09 <mcash0907@gmail.com>
2022-03-27 18:20:22 +0530

    final project files added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import NavBar from "./components/NavBar";
+import ProductDetails from "./components/ProductDetails";
+import Cart from "./components/Cart";
+import Payments from "./components/Payments";
+import {  Route } from "react-router-dom";
+import { GetProduct } from "./actions";
+import Carousell from "./components/Carousel";
+import TopCategories from "./components/TopCategories";
+import Footer from "./components/Footer";
+import ProductSection from "./components/ProductSection";
+import Overlay from "./components/Overlay";
+import ReactLoading from "react-loading";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const CartItems = useSelector((state) => state.CartItems);
+	const Products = useSelector((state) => state.Product);
+	const [drawer, setdrawer] = useState(false);
+
+	const dispatch = useDispatch();
+	useEffect(
+		() =>dispatch(GetProduct())
+		// eslint-disable-next-line
+	, []);
+
+	return (
+		<div className='App'>
+			<NavBar setDrawer={setdrawer} />
+			<Cart drawer={drawer} setdrawer={setdrawer} />
+      
+			<Route path='/' exact>
+				{Products.length > 0 ? (
+					<>
+						<Carousell />
+						<TopCategories />
+						{/* <Overlay /> */}
+						<ProductSection products={Products} />
+					</>
+				) : (
+					<div
+						className='d-flex justify-content-center align-items-center'
+						style={{ height: "100vh" }}
+					>
+						<ReactLoading
+							type='spokes'
+							color='#000'
+							height={"100px"}
+							width={"100px"}
+						/>
+					</div>
+				)}
+			</Route>
+
+			<Route path='/details'>
+				<ProductDetails setDrawer={setdrawer} />
+			</Route>
+			<Route path='/payment' exact>
+				<Payments cartItems={CartItems} />
+			</Route>
+     
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
